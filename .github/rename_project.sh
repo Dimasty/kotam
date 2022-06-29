4346f42a7ca467ff5a5f37b4e4941d46eeda3d47
Dimasty <Dimasty@users.noreply.github.com>
2022-06-29 22:28:56 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Dimasty"
+original_name="kotam"
+original_urlname="kotam"
+original_description="Awesome kotam created by Dimasty"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv kotam $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
